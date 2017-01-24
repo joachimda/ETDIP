ffd875c98f0f202a6bef4020458241c39d93b332
Joachim <joachimdamandersen@gmail.com>
2017-01-24 15:21:50 +0100

    added Nearest Neighbour Algorithm, enlarging image x3

--- lab1/Untitled.m
+++ lab1/Untitled.m
@@ -1,23 +1,42 @@
 %ETDIP1 - Digital Image Processing
-%LAB exercise 1
-%
+%LAB 1
 
 %Close all open windows
 clear, close all;
 
+% Ex 1.
 fileName = 'pcameraman.tiff';
 img = imread(fileName);
 
-fftImg = fft(img);
-fft2Img = fft2(img);
-ifftImg = ifft(fftImg);
+%Determine image dimensions
+whos img;
 
-%Plot the samples
+%Determine pixel range
+min(img(:));
+max(img(:));
+
+%Show pixels above mean value
+m = mean(img(:));
+aboveMeanImg = zeros(size(img));
+%lr = find(img > m); aboveMean
+
+
+%Plot the image
 figure
 subplot(2,2,1);imshow(img);title('Original sample');
-subplot(2,2,2);imshow(fftImg);title('FFT sample');
-subplot(2,2,3);imshow(fft2Img);title('FFT2 sample');
-subplot(2,2,4);imshow(ifftImg);title('Invers FFT from FFT sample');
+subplot(2,2,2);imshow(img(100:150,100:150,:));title('Part of image (100,150);(100,150)');
+subplot(2,2,3);hist(double(img(:)));
 
-figure
-subplot(2,2
+%Triple the size of the image using Nearest Neighour"
+E = 3;
+imgTripled = zeros(size(img)*E);
+for r = 1:size(imgTripled,2)
+    for s = 1:size(imgTripled,2)
+        imgTripled(r, s) = img(ceil(r/E),ceil(s/E));
+    end;
+end;
+
+%Show the enlarged image
+figure(6)
+imshow(uint8(imgTripled));
+title('Image enlarged x3 - Neares neighbour');
