7970e83eafc3023dfcc00a1f8b487bb297a79f3f
Joachim <joachimdamandersen@gmail.com>
2017-01-31 12:31:19 +0100

    added lab3 script. Renamed older scripts

--- lab2/lab2script.m
+++ lab2/lab2script.m
@@ -6,3 +6,24 @@
 clear, close all;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+fileName = 'pcameraman.tiff';
+img = imread(fileName);
+
+dI=double(img);
+dI=dI/max(dI(:));
+
+%%Blurring the image
+M=9;
+h=ones(1,M);
+h=h./sum(h(:));
+
+for r=1:size(dI,1)
+    dJ(r,:)=filter(h,1,dI(r,:));
+end;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+dJ=dJ+.0*randn(size(dJ));
+
+figure
+subplot(2,2,1);imshow(dI),title('Original image');
+subplot(2,2,2);imshow(dJ),title('Blurred image');
